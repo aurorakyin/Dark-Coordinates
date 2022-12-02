3abea17cba780f5b5c37b23fe0f6ad5dd6426d44
aurorakyin <112678275+aurorakyin@users.noreply.github.com>
2022-12-02 01:37:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import os
+import sqlite3
+connection = sqlite3.connect(“user.db”)
 
-# can we import from cs50?
-from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-
-from helpers import apology, login_required, lookup, usd
 
 # Configure application
 app = Flask(__name__)
