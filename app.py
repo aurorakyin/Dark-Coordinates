8a1a95e29cc525427b611eca904f8b1c80c182f8
anthonyrodmir <tony04rod@gmail.com>
2022-12-06 02:04:06 -0500

    edits from finance

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import os
 import sqlite3
-connection = sqlite3.connect(“user.db”)
+connection = sqlite3.connect("user.db")
 
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
+# from helpers import requests
+
 
 # Configure application
 app = Flask(__name__)
 
 # Custom filter
-app.jinja_env.filters["usd"] = usd
+# app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
@@ -19,7 +21,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+# db = SQL("sqlite:///finance.db")
 
 
 @app.after_request
