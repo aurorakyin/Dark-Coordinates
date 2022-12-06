37ef3a46436a3081568c50548ebc919a07a7eb8e
aurorakyin <112678275+aurorakyin@users.noreply.github.com>
2022-12-06 17:42:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-import os
-import sqlite3
-connection = sqlite3.connect("user.db")
+pip install cs50
 
+import os
+
+from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 # from helpers import requests
 
+db = darkcoor.db
 
 # Configure application
 app = Flask(__name__)
